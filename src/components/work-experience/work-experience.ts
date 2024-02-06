7df85b50dc2de676cad4b3c28c17a61754772afa
bbronswijk <brambronswijk@gmail.com>
2024-02-06 21:57:58 +0100

    #0: moved custom css to tailwind and improved content

--- src/components/work-experience/work-experience.ts
+++ src/components/work-experience/work-experience.ts
@@ -1,23 +1,22 @@
 const containerEl = document.querySelector('.js-client-illustration') as HTMLElement;
 
-const callBackFunction = ([{target, isIntersecting}]: IntersectionObserverEntry[]) => {
-  if (!(target instanceof HTMLElement)) {
-    return;
-  }
+const observer = new IntersectionObserver(
+  ([{target, isIntersecting}]: IntersectionObserverEntry[]) => {
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
 
-  isIntersecting
-    ? target.classList.add('active')
-    : target.classList.remove('active');
+    isIntersecting
+      ? target.classList.add('active')
+      : target.classList.remove('active');
 
-  containerEl.dataset.active = isIntersecting
-    ? target.dataset.client
-    : target.dataset.prev;
-};
-
-const observer = new IntersectionObserver(callBackFunction, {
-  rootMargin: '300% 0% -50% 0%'
-});
+    containerEl.dataset.active = isIntersecting
+      ? target.dataset.client
+      : target.dataset.prev;
+  }, {
+    rootMargin: '300% 0% -50% 0%'
+  });
 
 document
-  .querySelectorAll('small:first-child')
+  .querySelectorAll('.js-client-period')
   .forEach((i: Element) => observer.observe(i));
---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import plugin from 'tailwindcss/plugin';
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				layout: `
+					minmax(var(--padding-inline), 1fr)
+					min(50% - var(--padding-inline), var(--content-max-width) / 2)
+					min(50% - var(--padding-inline), var(--content-max-width) / 2)
+					minmax(var(--padding-inline), 1fr);
+				`,
+			},
+			grayscale: {
+				80: '80%',
+			},
+			textShadow: {
+				DEFAULT: '0 4px 1em hsl(267 57% 63% / 0.6)'
+			},
+			colors: {
+				bg: 'hsl(249 22% 12% / <alpha-value>)',
+				card: 'hsl(249 22% 20% / <alpha-value>)',
+			},
+			animation: {
+				fade: 'fadeIn .2s ease-in-out',
+			},
+			keyframes: {
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+			},
+		},
+	},
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') }
+			)
+		}),
+	],
+};
